71e2aff862c84b6764c798243e2207ed7ab9b0ef
MSK MyPC_Asus <saddek.kadji@gmail.com>
2016-11-27 12:14:53 +0100

    add all files

---
+++ sqlite/sqlite/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace sqlite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btngo = new System.Windows.Forms.Button();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.CreateDB = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btngo
+            // 
+            this.btngo.Location = new System.Drawing.Point(70, 386);
+            this.btngo.Name = "btngo";
+            this.btngo.Size = new System.Drawing.Size(75, 23);
+            this.btngo.TabIndex = 0;
+            this.btngo.Text = "Go";
+            this.btngo.UseVisualStyleBackColor = true;
+            this.btngo.Click += new System.EventHandler(this.btngo_Click);
+            // 
+            // grid
+            // 
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Location = new System.Drawing.Point(70, 30);
+            this.grid.Name = "grid";
+            this.grid.Size = new System.Drawing.Size(498, 325);
+            this.grid.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(493, 386);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // CreateDB
+            // 
+            this.CreateDB.Location = new System.Drawing.Point(320, 414);
+            this.CreateDB.Name = "CreateDB";
+            this.CreateDB.Size = new System.Drawing.Size(75, 23);
+            this.CreateDB.TabIndex = 3;
+            this.CreateDB.Text = "CreateDB";
+            this.CreateDB.UseVisualStyleBackColor = true;
+            this.CreateDB.Click += new System.EventHandler(this.CreateDB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 505);
+            this.Controls.Add(this.CreateDB);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.grid);
+            this.Controls.Add(this.btngo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btngo;
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button CreateDB;
+    }
+}
+
---
+++ sqlite/sqlite/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Windows.Forms;
+using System.IO;
+
+namespace sqlite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            createAboutDatabase();
+        }
+
+        private void createAboutDatabase()
+        {
+            const string DataBaseFileName = @"D:\Documents_New_PC\Training\c#\sqlite\sqlite\localDB\SQLite\About.sqlite";
+            File.Delete(DataBaseFileName);
+            SQLiteConnection.CreateFile(DataBaseFileName);
+
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source="+DataBaseFileName+"; Version=3;");
+            m_dbConnection.Open();
+
+            //string sqlResuest = "CREATE TABLE friends(id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(20), lastname VARCHAR(20), age INTEGER )";
+            string sqlResuest = "CREATE TABLE friends(firstname VARCHAR(20), lastname VARCHAR(20), age INTEGER )";
+            SQLiteCommand command = new SQLiteCommand(sqlResuest, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sqlResuest = "INSERT INTO friends (firstname, lastname,age ) values ('KADJI','Saddek', 31)";
+
+            command = new SQLiteCommand(sqlResuest, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            m_dbConnection.Close();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btngo_Click(object sender, EventArgs e)
+        {
+            const string filename = @"D:\Documents_New_PC\Training\c#\sqlite\sqlite\localDB\SQLite\About.sqlite";
+            const string sql = "select * from friends;";
+            var conn = new SQLiteConnection("Data Source=" + filename + ";Version=3;");
+            try
+            {
+                conn.Open();
+                DataSet ds = new DataSet();
+                var da = new SQLiteDataAdapter(sql, conn);
+                da.Fill(ds);
+                grid.DataSource = ds.Tables[0].DefaultView;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void CreateDB_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection.CreateFile("MyDatabase.sqlite");
+
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+
+            string sql = "create table highscores (name varchar(20), score int)";
+
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into highscores (name, score) values ('Me', 9001)";
+
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            m_dbConnection.Close();
+        }
+    }
+}
